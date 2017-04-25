dd57cfbaa5dcf207de9e00f57cc282d80425cd73
njmo <mklonica94@gmail.com>
2017-04-25 22:10:07 +0200

    integrated corelation between test and application

--- rpi/tests/OtherBasicTest/BasicTest.cpp
+++ rpi/tests/OtherBasicTest/BasicTest.cpp
@@ -1,9 +1,11 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "someheader.hpp"
+#include "testClass.h"
 
 TEST(MyString, ConstructorFromCString) {
 	A a;
+	testClass testc;
   EXPECT_EQ(0, 0);
 
 }
